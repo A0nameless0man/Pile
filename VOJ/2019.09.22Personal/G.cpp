42610e434ec467b402ba00508220eb6c4f618f59
Hg@SurfaceBook <1395943920@qq.com>
2019-09-22 14:56:41 +0800

    09/22/2019 14:56:40

---
+++ VOJ/2019.09.22Personal/G.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include<cmath>
+#include <unordered_map>
+using namespace std;
+double sq(double i) { return i * i; }
+bool match(double T, double V1, double V2)
+{
+    double r = V2 / V1;
+    if (V2 >( V1 * 3))
+    {
+        return false;
+    }
+    else
+    {
+        if (V2 > (V1 * 1.4142135623730951))
+        {
+            double x = 300 * ((sq(r) + 2) - sqrt(-sq(sq(r))+10*sq(r)-4+0.0001)) / (sq(r) - 1);
+            double ETAA = (x + 4 * 300) / V1;
+            double ETAS = (2 * 300 - x) / V2 + T;
+            return ETAA <= ETAS;
+        }
+        else
+        {
+            double x = 300 * (sq(r) - 1 - 2 * sqrt(1 - sq(1 - sq(r))+0.0001)) / (2 * (1 - sq(r)));
+            double ETAA = (x + 2 * 300) / V1;
+            double ETAS = (2 * 300 - x) / V2 + T;
+            return ETAA <= ETAS;
+        }
+    }
+}
+int main(void)
+{
+    int T;
+    std::cin >> T;
+    int t = 0;
+    while (T--)
+    {
+        double T, V1, V2;
+        std::cin >> T >> V1 >> V2;
+        bool ans = match(T,V1,V2);
+
+        std::cout << "Case #" << ++t << ": ";
+        if (ans)
+        {
+            std::cout << "Yes";
+        }
+        else
+        {
+            std::cout << "No";
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ VOJ/2019.09.22Personal/H.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+//std::unordered_map<char , int[2]> DIR = {{'U',{-1,0}},{'D',{1,0}},{'L',{0,-1}},{'R',{0,1}}};
+bool graid[1007][1007] = {0};
+int sqrt(int i)
+{
+    return i * i;
+}
+void tryMove(int &x, int &y, const char o)
+{
+    int nx = x, ny = y;
+    switch (o)
+    {
+    case 'U':
+        nx--;
+        break;
+    case 'D':
+        nx++;
+        break;
+    case 'L':
+        ny--;
+        break;
+    case 'R':
+        ny++;
+        break;
+    default:
+        break;
+    }
+    if (graid[nx][ny])
+    {
+        return;
+    }
+    else
+    {
+        x = nx;
+        y = ny;
+        return;
+    }
+}
+
+int main(void)
+{
+    int T;
+    std::cin >> T;
+    int t =0;
+    while (T--)
+    {
+        int n, m, o, l;
+        std::cin >> n >> m >> o >> l;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                graid[i][j] = false;
+            }
+        }
+        for (int i = 0; i <= n + 1; i++)
+        {
+            graid[i][0] = graid[i][m + 1] = true;
+        }
+        for (int j = 0; j <= m + 1; j++)
+        {
+            graid[0][j] = graid[n + 1][j] = true;
+        }
+        for (int i = 0; i < o; i++)
+        {
+            int x, y;
+            std::cin >> x >> y;
+            graid[x][y] = true;
+        }
+        std::string order;
+        std::cin >> order;
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (!graid[i][j])
+                {
+                    int cx = i, cy = j;
+                    for (auto c : order)
+                    {
+                        tryMove(cx, cy, c);
+                    }
+                    ans += sqrt(i - cx) + sqrt(j - cy);
+                }
+            }
+        }
+        std::cout << "Case #"<<++t<<": "<<ans << std::endl;
+    }
+}
