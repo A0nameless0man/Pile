2b36fde1db38d23c5c63d20ed3cac1b4a83f8ce6
Hg@SurfaceBook <1395943920@qq.com>
2019-10-19 00:23:10 +0800

    tiredTree

--- 洛谷/P2414打字机/ACmachine.cpp
+++ 洛谷/P2414打字机/ACmachine.cpp
@@ -1,8 +1,20 @@
-#include<cstdio>
-#include<string>
+#include <cstdio>
+#include <vector>
+#include <string>
 class TireTree
 {
 private:
+    struct node
+    {
+        node *childs[26] = {NULL};
+        node *fail = NULL;
+        int counter = 0;
+    } root;
 
-
+public:
+    template <class StringVec = std::vector<std::string>>
+    TireTree(StringVec vec,bool allowConflict = false);
+    bool contains(std::string str);
+    bool erase(std::string);
+    bool insert(std::string);
 };
