dcd5fc2faf66c87bb0a62699cd37a036c427da08
Hg@SurfaceBook <1395943920@qq.com>
2019-10-24 10:17:31 +0800

    10/24/2019 10:17:30

--- CodeForces/Round#591/D.cpp
+++ CodeForces/Round#591/D.cpp
@@ -3,7 +3,8 @@
 #include<map>
 size_t n;
 std::vector<size_t> raw;
-std::vector<size_t> next;//next[l] is the smallest r that raw[l:r] is leagel
+std::vector<size_t> next;//next[l] is the number of r that raw[l:r) is leagel
+std::vector<size_t> nxt;//nex[l] is the smallest 
 void doNext(void)
 {
     next.clear();
