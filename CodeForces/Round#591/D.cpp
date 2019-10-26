46595acbccf2de2921750c142e5124432e153c87
HG <1395943920@qq.com>
2019-10-26 16:56:17 +0800

    10/26/2019 16:56:16

--- CodeForces/Round#591/D.cpp
+++ CodeForces/Round#591/D.cpp
@@ -2,11 +2,15 @@
 #include<vector>
 #include<map>
 size_t n;
+using NextX = std::map<size_t,size_t>;
 std::vector<size_t> raw;
 std::vector<size_t> next;//next[l] is the number of r that raw[l:r) is leagel
-std::vector<size_t> nxt;//nex[l] is the smallest 
+std::vector<size_t> count;
 void doNext(void)
 {
-    next.clear();
-    next.resize(n,-1);
+    size_t i = n;
+    while(i--)
+    {
+        
+    }
 }
