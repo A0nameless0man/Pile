7b617064b567665e0ce12a1b73549e55dd0c3ddd
Hg@SurfaceBook <1395943920@qq.com>
2019-11-08 21:16:00 +0800

    acm

--- Algorithms/StringAlgorithm/AC_Machine/ACM.cpp
+++ Algorithms/StringAlgorithm/AC_Machine/ACM.cpp
@@ -1,10 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <cstring>
 #include <iostream>
 #include <queue>
-const size_t MAX_N = 100007;
-const size_t MAX_M = 26;
-const size_t INF = -1;
-size_t ctoi(char &c)
+using ULL = unsigned long long;
+const ULL MAX_N = 100007;
+const ULL MAX_M = 26;
+const ULL INF = -1;
+ULL ctoi(char &c)
 {
     return c - 'a';
 }
@@ -12,19 +14,20 @@ struct Trie
 {
     struct Node
     {
-        size_t next[MAX_M];
-        size_t fail;
-        size_t count;
+        ULL next[MAX_M];
+        ULL fail;
+        ULL count;
     } nodes[MAX_N];
-    size_t root, cnt;
-    void Tire(size_t n = MAX_N)
+    ULL root, cnt;
+    void Tire(ULL n = MAX_N)
     {
-        cnt = 0;root = newNode();
+        cnt = 0;
+        root = newNode();
     }
-    size_t newNode()
+    ULL newNode()
     {
         Node &cur = nodes[cnt];
-        for (size_t i = 0; i < MAX_M; i++)
+        for (ULL i = 0; i < MAX_M; i++)
         {
             cur.next[i] = INF;
         }
@@ -33,13 +36,13 @@ struct Trie
         ++cnt;
         return cnt - 1;
     }
-    void insert(char buf[],size_t id)
+    void insert(char buf[])
     {
-        size_t len = strlen(buf);
-        size_t cur = root;
-        for (size_t i = 0; i < len;i++)
+        ULL len = strlen(buf);
+        ULL cur = root;
+        for (ULL i = 0; i < len; i++)
         {
-            size_t &next = nodes[cur].next[ctoi(buf[i])];
+            ULL &next = nodes[cur].next[ctoi(buf[i])];
             if (next == INF)
             {
                 next = newNode();
@@ -50,21 +53,94 @@ struct Trie
     }
     void buildFailTree(void)
     {
-        std::queue<size_t> que;
+        std::queue<ULL> que;
         nodes[root].fail = root;
-        for (size_t i = 0; i < MAX_M; i++)
+        auto op = [this, &que](const ULL &tar) -> void {
+            for (ULL i = 0; i < MAX_M; i++)
+            {
+                if (nodes[tar].next[i] == INF)
+                {
+                    nodes[tar].next[i] = tar==root?root:nodes[nodes[tar].fail].next[i];
+                }
+                else
+                {
+                    nodes[nodes[tar].next[i]].fail = tar==root?root:nodes[nodes[tar].fail].next[i];
+                    que.push(nodes[tar].next[i]);
+                }
+            }
+        };
+        op(root);
+        while(!que.empty())
         {
-            if(nodes[root].next[i]==INF)
-            {
-                nodes[root].next[i] = root;
-            }
-            else
-            {
-                nodes[nodes[root].next[i]].fail = root;
-                
-            }
-            
+            op(que.front());
+            que.pop();
         }
-        
+    }
+    int query(char buf[])
+    {
+        ULL len = strlen(buf);
+        ULL cur = root;
+        ULL ans =0;
+        for (ULL i = 0; i < len; i++)
+        {
+            cur = nodes[cur].next[ctoi(buf[i])];
+            ULL tmp = cur;
+            while (tmp!=root)
+            {
+                ans+=nodes[tmp].count;
+                tmp = nodes[tmp].fail;
+            }
+        }
+        return ans;
+    }
+    void debug(void)
+    {
+        for(ULL i = 0;i<cnt;i++)
+        {
+            printf("{\"id\" : %3llu , \"fail\" : %3llu , \"chi\" : [",i,nodes[i].fail);
+            for(ULL j = 0;j<MAX_M;j++)
+            {
+                printf(" %3llu ,",nodes[i].next[j]);
+            }
+            printf("],\n");
+        }
     }
 };
+char buf[MAX_N];
+Trie acm;
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int m;
+        scanf("%d",&m);
+        acm.Tire();
+        while(m--)
+        {
+            scanf("%s",buf);
+            acm.insert(buf);
+        }
+        acm.buildFailTree();
+        scanf("%s",buf);
+        printf("%d match found.\n",acm.query(buf));
+        // acm.debug();
+    }
+    return 0;
+}
+/*
+10
+10
+a
+b
+ab
+c
+d
+cd
+abcd
+e
+f
+ef
+abcdef
+*/
