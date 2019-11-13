6a4f30e7b1bc85d70c316c71a375e6bcfd4cea1f
Hg@SurfaceBook <1395943920@qq.com>
2019-11-13 23:48:54 +0800

    Improve posture level

---
+++ vector_bool.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+int main(void)
+{
+    std::vector<int> veci;
+    std::vector<bool> vecb;
+    std::vector<std::vector<int>> vecv;
+    veci.resize(10);
+    vecb.resize(10);
+    vecv.resize(10);
+    int cnt = 0;
+    for(auto i:veci)
+    {
+        i = cnt++;
+    }
+    for(auto i:veci)
+    {
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+    vecb[8] = 15;
+    // for(auto i:vecb)
+    // {
+    //     i = cnt%2==0;
+    //     cnt++;
+    // }
+    for(auto i:vecb)
+    {
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+
+    
+    return 0;
+}
