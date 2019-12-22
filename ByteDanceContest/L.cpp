567e23b86c00a373d99ecdae74901c525623768c
HG <1395943920@qq.com>
2019-12-22 20:55:59 +0800

    ByteDance

---
+++ ByteDanceContest/L.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+char buf[2000007];
+int oldmain(void)
+{
+    int n, k;
+    while (scanf("%d%d%s", &n, &k, buf) != EOF)
+    {
+        bool add = false;
+        for (int i = k; i < n; ++i)
+        {
+            if (buf[i] > buf[i % k])
+            {
+                add = true;
+            }
+        }
+        if (add)
+        {
+            buf[k - 1]++;
+            for (int i = k - 1; i > 0; --i)
+            {
+                if (buf[i] > '9')
+                {
+                    ++buf[i - 1];
+                    buf[i] = '0';
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            buf[i] = buf[i % k];
+        }
+        printf("%d\n%s\n", n, buf);
+        //putchar('\n');
+    }
+    return 0;
+}
+#include <iostream>
+#include <string>
+int main(void)
+{
+    std::string x, y;
+    int n, k;
+    while (std::cin >> n >> k >> x)
+    {
+        y.clear();
+        for (int i = 0; i < n; ++i)
+        {
+            y.push_back(x[i % k]);
+        }
+        if (y < x)
+        {
+            ++(y[k - 1]);
+            for (int i = k - 1; i > 0; --i)
+            {
+                if (y[i] > '9')
+                {
+                    y[i] = '0';
+                    ++(y[i - 1]);
+                }
+            }
+            for (int i = k; i < n; ++i)
+            {
+                y[i] = y[i % k];
+            }
+        }
+        std::cout << n << std::endl
+                  << y << std::endl;
+    }
+}
---
+++ ByteDanceContest/M.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+int main(void)
+{
+    int n, m;
+    std::vector<unsigned> a, b;
+    while (std::cin >> n >> m)
+    {
+        a.clear();
+        b.clear();
+        for (int i = 0; i < n; ++i)
+        {
+            int ai;
+            //std::cin >> ai;
+            scanf("%d", &ai);
+            a.push_back(ai % m);
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            int bi;
+            //std::cin >> bi;
+            scanf("%d", &bi);
+            b.push_back(bi % m);
+        }
+        std::sort(a.begin(), a.end());
+        std::sort(b.begin(), b.end());
+        int predictX;
+        int x = 0x7fffffff;
+        //while (!found)
+        for (int offset = 0; offset < n; ++offset)
+        {
+            predictX = ((b[offset] + m) - a[0]) % m;
+            for (int i = 0; i < n; ++i)
+            {
+                if ((a[i] + predictX) % m != b[(i + offset) % n] % m)
+                {
+                    predictX = 0x7fffffff;
+                    break;
+                }
+            }
+            x = std::min(x, predictX);
+        }
+        std::cout << x << std::endl;
+    }
+}
