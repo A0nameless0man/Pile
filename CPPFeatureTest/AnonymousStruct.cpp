329755f4bb18f727b1f1b61f5db539f88681edce
HG <1395943920@qq.com>
2020-01-02 20:44:02 +0800

    decltype and test AnonymousStruct

--- CPPFeatureTest/AnonymousStruct.cpp
+++ CPPFeatureTest/AnonymousStruct.cpp
@@ -10,13 +10,17 @@ struct
     int y;
 }b;
 
+using C = decltype(b);
+
 int main(void)
 {
     A a;
+    C c;
     //a2.x = 0;
     a.x = 0;
     b.y = 0;
     std::cout << typeid(a).name() << std::endl;
     //std::cout << typeid(a2).name() << std::endl;
     std::cout << typeid(b).name() << std::endl;
+    std::cout << typeid(c).name() << std::endl;
 }
