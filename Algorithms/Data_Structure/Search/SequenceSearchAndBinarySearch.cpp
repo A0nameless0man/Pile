30bf5fb508a7efeb5ddddbc334b1fd9bb6160cad
HG <1395943920@qq.com>
2020-01-06 10:13:54 +0800

    Killed

--- Algorithms/Data_Structure/Search/SequenceSearchAndBinarySearch.cpp
+++ Algorithms/Data_Structure/Search/SequenceSearchAndBinarySearch.cpp
@@ -2,23 +2,19 @@
 #include <chrono>
 #include <iomanip>
 #include <iostream>
-#include <random>
-#include <time.h>
 #include <vector>
 
-template <class T>
-std::chrono::duration<double> timing(T t)
+struct myclock
 {
-    auto cstart = std::chrono::system_clock::now();
-    t();
-    auto cend = std::chrono::system_clock::now();
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> timeConsumption = (cend - cstart);
-    return timeConsumption;
-}
-
-const size_t MAX_N = 100000000;
-
+    template <typename A = std::chrono::microseconds>
+    auto count()
+    {
+        return std::chrono::duration_cast<A>(std::chrono::steady_clock::now() - start).count();
+    }
+};
+using namespace std;
 template <typename T>
 size_t sequenceFind(std::vector<T> &vec, const T &key)
 {
@@ -52,6 +48,7 @@ std::pair<size_t, size_t> binarySearch(std::vector<T> &vec, const T &key)
     while(l < r)
     {
         size_t mid = (l + r) / 2;
+        // std::cout << "mid: " << mid << std::endl;
         if(vec[mid] > key)
         {
             r = mid - 1;
@@ -64,39 +61,49 @@ std::pair<size_t, size_t> binarySearch(std::vector<T> &vec, const T &key)
     }
     return { l, count };
 }
+const size_t MAX_N = 100000000;
+const size_t TIMES = 1000;
+
 int main(void)
 {
-    std::vector<size_t> a;
-    a.reserve(MAX_N + 10000);
-    for(size_t i = 0; i < MAX_N; ++i)
+     size_t n;
+    while(std::cin >> n)
     {
-        a.push_back(i);
-    }
-    // TIMING(std::shuffle(a.begin(), a.end(), std::random_device()))
-    std::chrono::duration<double> s;
-    std::chrono::duration<double> g;
-    std::chrono::duration<double> b;
-    for(int i = 0; i < 1000; ++i)
-    {
-        s += timing([&]() -> void {
-            sequenceFind(a, MAX_N / 2);
-        });
-    }
-    std::cout << "s" << std::fixed << s.count() << std::endl;
-    for(int i = 0; i < 1000; ++i)
-    {
-        b += timing([&]() -> void {
-            binarySearch(a, MAX_N / 2);
-        });
-    }
-    std::cout << "b" << std::fixed << b.count() << std::endl;
-    sequenceFind(a, MAX_N);
-    for(int i = 0; i < 1000; ++i)
-    {
-        g += timing([&]() -> void {
-            guardedSequenceFind(a, MAX_N/2);
-        });
-    }
-    std::cout << "g" << std::fixed << g.count() << std::endl;
+        std::vector<size_t> a;
+        a.reserve(n + 10000);
+        for(size_t i = 0; i < n; ++i)
+        {
+            a.push_back(i);
+        }
+        
+        size_t tar;
+        std::cin >> tar;
 
+        {
+            myclock c;
+
+            for(size_t i = 0; i < TIMES; ++i) {
+                // sequenceFind(a, tar);
+                auto it = std::find(a.begin(), a.end(), tar);
+            }
+                
+            cout << c.count() << endl;
+        }
+        {
+            myclock c;
+ 
+            for(size_t i = 0; i < TIMES; ++i) {
+                // binarySearch(a, tar);
+                auto it = std::lower_bound(a.begin(), a.end(), tar);
+            }
+            cout << c.count() << endl;
+        }
+        // {
+        //     myclock c;
+
+        //     for(size_t i = 0; i < TIMES; ++i)
+        //         guardedSequenceFind(a, tar);
+        //     cout << c.count() << endl;
+        // }
+    }
 }
