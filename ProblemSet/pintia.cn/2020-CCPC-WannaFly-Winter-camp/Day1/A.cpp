9513bc29f95ec78f0a6332b037891b505cb63895
Hg@SurfaceBook <1395943920@qq.com>
2020-01-13 16:14:16 +0800

    n*sqrt(n)*26>1e9?

---
+++ ProblemSet/pintia.cn/2020-CCPC-WannaFly-Winter-camp/Day1/A.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+const long long MAX_N = 1e6 + 7;
+char            buf[MAX_N];
+
+int ctoi(char c)
+{
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+std::vector<long double> prefix1, prefix2;
+
+int main(void)
+{
+    {
+        long double sum1 = 0;
+        //, sum2 = 0;
+
+        for(long long i = 1; i < MAX_N; i++)
+        {
+            sum1 += 1.0 / i;
+            prefix1.push_back(sum1);
+        }
+    }
+    while(scanf("%s", buf) != EOF)
+    {
+        char *           p = buf;
+        std::vector<int> vec;
+        while(*p)
+        {
+            vec.push_back(ctoi(*p));
+            ++p;
+        }
+        long double cnt = 0;
+        long long   sum = [](size_t n) -> long long {
+            long long sum = 0;
+            for(long long i = 1; i <= n; ++i)
+            {
+                long long da = i;  //* (n - i + 1);
+                sum += da;
+            }
+            return sum;
+        }(vec.size());
+        long long n = vec.size();
+
+        {
+            prefix2.clear();
+            long double sum2 = 0;
+            for(int i = 1; i < n; ++i)
+            {
+                prefix2.push_back(sum2);
+                sum2 += 1.0 * i / (n - i+1);
+            }
+        }
+        for(long long i = 0; i < vec.size(); ++i)
+        {
+            // cnt += vec[i] * (i + 1) * (vec.size() - i);
+            if(vec[i])
+            {
+                cnt += std::min(i + 1, n - i);  //[1,min(i,n-i+1) )
+                cnt += (prefix1[std::max(i + 1, n - i) - 1] - prefix1[std::min(i + 1, n - i) - 1]) *
+                       std::min(i + 1, n - i);  //[min ,max)
+                cnt += prefix2[n - std::max(i + 1, n - i)];
+            }
+        }
+        // std::cout << cnt << "/" << sum << std::endl;
+        std::cout << std::setprecision(11)
+                  << static_cast<long double>(cnt) / static_cast<long double>(sum) << std::endl;
+    }
+}
---
+++ ProblemSet/pintia.cn/2020-CCPC-WannaFly-Winter-camp/Day1/K.cpp
@@ -0,0 +1,187 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <string>
+
+using ULL = unsigned long long;
+
+const ULL MAX_N = 1000007;
+const ULL MAX_M = 26;
+const ULL INF   = static_cast<ULL>(-1);
+
+ULL ctoi(const char &c)
+{
+    return c - 'a';
+}
+struct Trie
+{
+    struct Node
+    {
+        ULL next[MAX_M];
+        ULL fail;
+        ULL count;
+        ULL cost;
+        ULL len;
+    } nodes[MAX_N];
+    ULL  root, cnt;
+    void clear(void)
+    {
+        cnt  = 0;
+        root = newNode();
+    }
+    ULL newNode()
+    {
+        Node &cur = nodes[cnt];
+        for(ULL i = 0; i < MAX_M; i++)
+        {
+            cur.next[i] = INF;
+        }
+        cur.fail  = INF;
+        cur.count = 0;
+        cur.cost  = INF;
+        ++cnt;
+        return cnt - 1;
+    }
+    void insert(const char buf[], ULL cost = 0)
+    {
+        ULL len = strlen(buf);
+        ULL cur = root;
+        for(ULL i = 0; i < len; i++)
+        {
+            ULL &next = nodes[cur].next[ctoi(buf[i])];
+            if(next == INF)
+            {
+                next = newNode();
+            }
+            cur = next;
+        }
+        nodes[cur].count++;
+        nodes[cur].cost = std::min(nodes[cur].cost, cost);
+        nodes[cur].len  = len;
+    }
+    void buildFailTree(void)
+    {
+        std::queue<ULL> que;
+        nodes[root].fail = root;
+        auto op          = [this, &que](const ULL &tar) -> void {
+            for(ULL i = 0; i < MAX_M; i++)
+            {
+                if(nodes[tar].next[i] == INF)
+                {
+                    nodes[tar].next[i] = tar == root ? root : nodes[nodes[tar].fail].next[i];
+                }
+                else
+                {
+                    nodes[nodes[tar].next[i]].fail =
+                      tar == root ? root : nodes[nodes[tar].fail].next[i];
+                    que.push(nodes[tar].next[i]);
+                }
+            }
+        };
+        op(root);
+        while(!que.empty())
+        {
+            op(que.front());
+            que.pop();
+        }
+    }
+    int query(const char buf[], ULL dp[])
+    {
+        ULL len = strlen(buf);
+        ULL cur = root;
+        for(ULL i = 0; i < len; ++i)
+        {
+            dp[i] = INF;
+        }
+        for(ULL i = 0; i < len; i++)
+        {
+            cur     = nodes[cur].next[ctoi(buf[i])];
+            ULL tmp = cur;
+            while(tmp != root)
+            {
+                // ans += nodes[tmp].count;
+                if(nodes[tmp].count)
+                {
+                    if(i >= nodes[tmp].len)
+                    {
+                        dp[i] = std::min(dp[i], dp[i - nodes[tmp].len] + nodes[tmp].cost);
+                    }
+                    else
+                    {
+                        dp[i] = nodes[tmp].cost;
+                    }
+                }
+                tmp = nodes[tmp].fail;
+            }
+        }
+        return 0;
+    }
+    void debug(void)
+    {
+        for(ULL i = 0; i < cnt; i++)
+        {
+            printf("{\"id\" : %3llu , \"fail\" : %3llu , \"chi\" : [", i, nodes[i].fail);
+            for(ULL j = 0; j < MAX_M; j++)
+            {
+                printf(" %3llu ,", nodes[i].next[j]);
+            }
+            printf("],\n");
+        }
+    }
+};
+char buf[MAX_N];
+ULL  dp[MAX_N];
+Trie acm;
+int  main(void)
+{
+    int n;
+    while(scanf("%d", &n) != EOF)
+    {
+        acm.clear();
+        while(n--)
+        {
+            int cost;
+            scanf("%s", buf);
+            std::cin >> cost;
+            acm.insert(buf, cost);
+        }
+        scanf("%s", buf);
+        acm.buildFailTree();
+        acm.query(buf, dp);
+        ULL len = strlen(buf);
+
+        if(dp[len - 1] != INF)
+        {
+            std::cout << dp[len - 1] << std::endl;
+        }
+        else
+        {
+            std::cout << -1 << std::endl;
+        }
+    }
+    return 0;
+}
+/*
+4
+ab 5
+cd 10
+abc 100
+d 1
+abcd
+3
+ab 10
+ab 5
+a 1
+ababa
+3
+ba 10
+ab 5
+a 1
+ababa
+3
+ba 10
+b 2
+a 1
+ababa
+*/
