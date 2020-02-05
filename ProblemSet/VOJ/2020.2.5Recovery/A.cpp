c00999c6074f3ca28853409040558138a1d8d11e
Hg@SurfaceBook <1395943920@qq.com>
2020-02-05 17:08:23 +0800

    02/05/2020 17:08:21

--- ProblemSet/VOJ/2020.2.5Recovery/A.cpp
+++ ProblemSet/VOJ/2020.2.5Recovery/A.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
-#include<set>
+#include <iostream>
+#include <set>
 
 int main(void)
 {
     int n;
-    while(std::cin>>n)
+    while(std::cin >> n)
     {
         std::set<int> bucket;
-        for(int i = 0; i < n;++i)
+        for(int i = 0; i < n; ++i)
         {
             int buf;
             std::cin >> buf;
-            if(buf!=0)
+            if(buf != 0)
             {
                 bucket.insert(buf);
             }
         }
+        std::cout << bucket.size() << std::endl;
     }
-}   }
 }
