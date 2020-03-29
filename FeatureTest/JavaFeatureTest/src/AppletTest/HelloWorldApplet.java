81b8756c2f711cd3758b860b11354d12c054a638
Hg@SurfaceBook <1395943920@qq.com>
2020-03-29 17:13:01 +0800

    applet sucks

---
+++ FeatureTest/JavaFeatureTest/src/AppletTest/HelloWorldApplet.java
@@ -0,0 +1,14 @@
+package AppletTest;
+import java.applet.*;
+import java.awt.*;
+
+public class HelloWorldApplet extends Applet {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8526721036437469915L;
+
+    public void paint(Graphics g) {
+        g.drawString("Hello World", 25, 50);
+    }
+}
