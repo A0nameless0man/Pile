7f9459e1a602c3f441e2724a519d60f53dedc6d7
Hg@SurfaceBook <1395943920@qq.com>
2020-04-02 18:49:30 +0800

    2020-49-02--18-49

--- ProblemSet/NUIST10thCPC/MyProblem/ExHard/gen.py
+++ ProblemSet/NUIST10thCPC/MyProblem/ExHard/gen.py
@@ -1,14 +1,35 @@
 import random
-scale = [[(5, 10)] * 20]*5+[[(10, 20)] * 20]*15+ [[(50, 50)] * 4]*3+ [[(60, 60)]]*3+[[(100, 100)]]+[[(200,50)]]+[[(50,200)]]
+
+rand = lambda i, j: random.randint(0, 1)
+
+
+def hack1(i, j):
+    if i > j:
+        return i % 2
+    else:
+        return j % 2
+
+def hack2(i, j):
+    return (i + j) % 2
+
+scale = [[(5, 10, rand)] * 20] * 2
+scale += [[(10, 20, rand)] * 20] * 15
+scale += [[(50, 50, rand)] * 4] * 3
+scale += [[(60, 60, rand)]] * 3
+scale += [[(500, 500, rand)]]*2
+scale += [[(500, 500, hack1)]]
+scale += [[(500, 500, hack2)]]
+scale += [[(1000, 250, rand)]]
+scale += [[(250, 1000, rand)]]
 
 for i in range(len(scale)):
     f = open("./TestData/" + str(i + 2) + ".in", mode="w+", encoding="UTF-8")
-    f.write(str(len(scale[i]))+"\n")
-    for case in (scale[i]):
+    f.write(str(len(scale[i])) + "\n")
+    for case in scale[i]:
         m = case[0]
         n = case[1]
         f.write(str(m) + " " + str(n) + "\n")
         for j in range(m):
             for k in range(n):
-                f.write(str(random.randint(0, 1)))
+                f.write(str(case[2](j,k)))
             f.write("\n")
--- ProblemSet/NUIST10thCPC/MyProblem/ExHard/std.cpp
+++ ProblemSet/NUIST10thCPC/MyProblem/ExHard/std.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <stack>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -15,11 +16,11 @@ int main(void)
     {
         for(int t = 0; t < T; ++t)
         {
-            std::vector<std::vector<int>> image;
-            std::vector<std::vector<int>> block;
-            std::vector<int>              adj;
-            int                           m, n;
-            bool                          found = false;
+            std::vector<std::vector<int>>                    image;
+            std::vector<std::vector<int>>                    block;
+            std::vector<std::tuple<int, int, int, int, int>> adj;
+            int                                              m, n;
+            bool                                             found = false;
             std::cin >> m >> n;
             block.resize(m + 2, std::vector<int>(n + 2, -1));
             image.resize(m + 2);
@@ -62,7 +63,7 @@ int main(void)
                             std::queue<std::pair<int, int>> que;
                             int                             num = adj.size();
                             que.push(std::make_pair(i, j));
-                            adj.emplace_back(image[i][j]);
+                            int maxi = i, mini = i, maxj = j, minj = j;
                             block[i][j] = num;
                             while(!que.empty())
                             {
@@ -79,10 +80,15 @@ int main(void)
                                        && block[next.first][next.second] == -1)
                                     {
                                         block[next.first][next.second] = num;
+                                        maxi                           = std::max(next.first, maxi);
+                                        mini                           = std::min(next.first, mini);
+                                        maxj = std::max(next.second, maxj);
+                                        minj = std::min(next.second, minj);
                                         que.push(next);
                                     }
                                 }
                             }
+                            adj.emplace_back(image[i][j], mini, maxi + 1, minj, maxj + 1);
                         }
                     }
                 }
@@ -91,22 +97,26 @@ int main(void)
                 // std::cout << adj.size() << std::endl;
                 for(int curb = 0; curb < adj.size(); ++curb)
                 {
-                    if(adj[curb] == 1)
+                    if(std::get<0>(adj[curb]) == 1)
                     {
                         std::vector<std::vector<int>> cblock;
-                        int                           cnt = 0;
-                        cblock.resize(m, std::vector<int>(n, -1));
-                        for(int i = 0; i < m; ++i)
+                        int                           cnt  = 0;
+                        int                           mini = std::get<1>(adj[curb]) - 1;
+                        int                           maxi = std::get<2>(adj[curb]) + 1;
+                        int                           minj = std::get<3>(adj[curb]) - 1;
+                        int                           maxj = std::get<4>(adj[curb]) + 1;
+                        cblock.resize(maxi - mini, std::vector<int>(maxj - minj, -1));
+                        for(int i = mini; i < maxi; ++i)
                         {
-                            for(int j = 0; j < n; ++j)
+                            for(int j = minj; j < maxj; ++j)
                             {
-                                if(cblock[i][j] == -1)
+                                if(cblock[i - mini][j - minj] == -1)
                                 {
                                     std::queue<std::pair<int, int>> que;
                                     int                             num = cnt;
                                     que.push(std::make_pair(i, j));
                                     cnt++;
-                                    cblock[i][j] = num;
+                                    cblock[i - mini][j - minj] = num;
                                     while(!que.empty())
                                     {
                                         auto cur = que.front();
@@ -115,13 +125,14 @@ int main(void)
                                         {
                                             auto next = std::make_pair(cur.first + DIR[k][0],
                                                                        cur.second + DIR[k][1]);
-                                            if(next.first >= 0 && next.first < m && next.second >= 0
-                                               && next.second < n
+                                            if(next.first >= mini && next.first < maxi
+                                               && next.second >= minj && next.second < maxj
                                                && (block[next.first][next.second] == curb)
                                                     == (block[cur.first][cur.second] == curb)
-                                               && cblock[next.first][next.second] == -1)
+                                               && cblock[next.first - mini][next.second - minj]
+                                                    == -1)
                                             {
-                                                cblock[next.first][next.second] = num;
+                                                cblock[next.first - mini][next.second - minj] = num;
                                                 que.push(next);
                                             }
                                         }
@@ -129,17 +140,17 @@ int main(void)
                                 }
                             }
                         }
-                        if(cnt > 3)
+                        if(cnt == 4)
                         {
                             found = true;
 #ifdef DEBUG
-                            std::cout << "Block " << curb << " is +1s" << std::endl;
+                            std::cout << "Block " << curb << " is +" << cnt << "s" << std::endl;
                             std::cout << "Cblock" << std::endl;
-                            for(int i = 0; i < m; ++i)
+                            for(int i = mini; i < maxi; ++i)
                             {
-                                for(int j = 0; j < n; ++j)
+                                for(int j = minj; j < maxj; ++j)
                                 {
-                                    std::cout << cblock[i][j];
+                                    std::cout << cblock[i - mini][j - minj];
                                 }
                                 std::cout << '\n';
                             }
@@ -155,7 +166,7 @@ int main(void)
                 }
                 else
                 {
-                    std::cout << "I had seen this a lot." << std::endl;
+                    std::cout << "I'm not a journalist, but I've seen too much." << std::endl;
                 }
             }
 #ifdef DEBUG
@@ -165,7 +176,14 @@ int main(void)
                 {
                     for(int j = 0; j < n; ++j)
                     {
-                        std::cout << image[i][j];
+                        if(image[i][j])
+                        {
+                            std::cout << "\033[40m \033[40m";
+                        }
+                        else
+                        {
+                            std::cout << "\033[47m \033[40m";
+                        }
                     }
                     std::cout << '\n';
                 }
