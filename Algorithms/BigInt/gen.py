c3cb12c504cd0881bc4188a54ed629d27256cd25
Hg@SurfaceBook <1395943920@qq.com>
2020-05-12 15:01:11 +0800

    prepare for test

---
+++ Algorithms/BigInt/gen.py
@@ -0,0 +1,3 @@
+import random
+
+print(''.join([str(random.randint(0,9))for i in range(10000000)]))
--- Algorithms/BigInt/main.cpp
+++ Algorithms/BigInt/main.cpp
@@ -1,15 +1,44 @@
 // #include<iostream>
 
-#include"BigInt.hpp"
+#include "BigInt.hpp"
+#include <chrono>
 
+using namespace std::chrono;
+
+template <class C = steady_clock>
+class timer
+{
+    using TimePoint = std::chrono::time_point<C>;
+    TimePoint start_;
+    TimePoint end_;
+
+public:
+    timer &begin(void)
+    {
+        this->start_ = C::now();
+        return *this;
+    }
+    timer &stop(void)
+    {
+        this->end_ = C::now();
+        return *this;
+    }
+    int64_t get(void)
+    {
+        return duration_cast<microseconds>(end_ - start_).count();
+    }
+};
+using GoodClock = timer<steady_clock>;
 int main(void)
 {
-    BigInt::BigInt a, b;
-    while(std::cin>>a>>b)
+    BigInt::BigInt a, b, c;
+    while(std::cin >> a >> b)
     {
-        // std::cout << a + b << std::endl;
-        // std::cout << a - b << std::endl;
-        std::cout << a * b << std::endl;
+        GoodClock gc;
+        gc.begin();
+        for(int i = 0; i < 10; ++i)
+            c = a + b;
+        gc.stop();
+        std::cout << gc.get() << std::endl;
     }
-
 }
