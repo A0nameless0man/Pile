fe3bb07058356108e2ac7c242df6ef5e73f1f7fe
Hg@SurfaceBook <1395943920@qq.com>
2020-05-21 16:06:19 +0800

    within 1 sec.

--- ProblemSet/NUIST10thCPC/OthersProblem/Study.py
+++ ProblemSet/NUIST10thCPC/OthersProblem/Study.py
@@ -1,11 +1,11 @@
 n, m = map(int, input().split())
 cnt = [0 for i in range(m)]
 for i in range(n):
-    t = [int(i) for i in input().split()]
-    for j in range(m - 1):
-        if t[j] != t[j + 1]:
-            cnt[j + 1] = 1
-cnt[0]=1
+    t = input().split()
+    for j in range(1, m):
+        if t[j] != t[j - 1]:
+            cnt[j] = 1
+cnt[0] = 1
 for i in range(1, m):
     cnt[i] = cnt[i] + cnt[i - 1]
 print(" ".join([str(i) for i in cnt]))
