c7cd25d7bb86791dc1a701afcf76c3e6357f975f
Hg@SurfaceBook <1395943920@qq.com>
2020-06-01 08:44:27 +0800

    going on

--- FunnyToys/AutoTetris/Tetries.h
+++ FunnyToys/AutoTetris/Tetries.h
@@ -1,9 +1,26 @@
 #pragma once
 #include <array>
+#include <map>
 
 namespace Tetries
 {
-enum Block
+template <typename T, std::size_t N, std::size_t... M>
+struct MatrixGen
+{
+    using Type = std::array<typename MatrixGen<T, M...>::Type, N>;
+};
+
+template <typename T, std::size_t N>
+struct MatrixGen<T, N>
+{
+    using Type = std::array<T, N>;
+};
+
+template <typename T, std::size_t... N>
+using Matrix = typename MatrixGen<T, N...>::Type;
+
+using byte = char;
+enum class Block
 {
     I = 0,
     J,
@@ -12,6 +29,23 @@ enum Block
     S,
     T,
     Z
+};
+enum class Rotate
+{
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+template <size_t wide, size_t hight>
+using GameGrade     = Matrix<byte, hight, wide>;  // It used to be short[hight][wide];
+using BlockPoint    = Matrix<byte, 2>;            // short[2];
+using BlockDescribe = Matrix<BlockPoint, 4>;      // BlockPoint[4];
+using BlockShape    = GameGrade<4, 4>;
+static const std::array<BlockShape, static_cast<size_t>(Block::Z)> blockShape = {
+    { {1, 1, 1, 1  },
+    {  { 1, 0, 0, 0, 1, 1, 1, 0 } },
+    { { 0, 0, 1, 0, 1, 1, 1, 0 } }
 };
 /*
 ----I
@@ -37,34 +71,18 @@ enum Block
 ----
 */
 
-template <typename T, std::size_t N, std::size_t... M>
-struct MatrixGen
-{
-    using Type = std::array<typename MatrixGen<T, M...>::Type, N>;
-};
-
-template <typename T, std::size_t N>
-struct MatrixGen<T,N>
-{
-    using Type = std::array<T, N>;
-};
-
-
-template<typename T,std::size_t ...N>
-using Matrix = typename MatrixGen<T,N...>::Type;
-
-template <size_t wide, size_t hight>
-using GameGrade     = Matrix<short,hight,wide>;// It used to be short[hight][wide];
-using BlockPoint    = Matrix<short,2>;//short[2];
-using BlockDescribe = Matrix<BlockPoint,4>;//BlockPoint[4];
-using BlockShape    = GameGrade<4, 4>;
 template <size_t wide, size_t hight>
 class Tetries
 {
 private:
     GameGrade<wide, hight> gameBoard;
+    Block                  next;
+    Tetries<wide, hight> & generateNext(void);
 
 public:
+    Block                 getNext(void);
+    bool                  tryPutNextAt(size_t x, size_t y, Rotate r);
+    Tetries<wide, hight> &putNextAt(size_t x, size_t y, Rotate r);
 };
 
 using StanderdTetries = Tetries<10, 20>;
