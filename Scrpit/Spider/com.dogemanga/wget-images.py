34bb852312729a9853cec3f1780ff10175b3839a
huguang@WSL.HG-Desktop <1395943920@qq.com>
2021-12-05 21:29:35 +0800

    progress bar

--- Scrpit/Spider/com.dogemanga/wget-images.py
+++ Scrpit/Spider/com.dogemanga/wget-images.py
@@ -1,13 +1,15 @@
 import logging
 import os
 from pathlib import Path
-from typing import Dict,  Optional, Tuple
+from typing import Dict, List,  Optional, Tuple
 import requests
 from lxml import etree
 import argparse
 from retry import retry
-from multiprocessing import Pool
+from multiprocessing import Pool, current_process
 from functools import partial
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 
 class HTTPException(Exception):
@@ -87,7 +89,7 @@ def download_section(path: str, name: str, url: str):
     logger.info("Start Downloading section url %s to %s" % (name, path))
     Path(path).mkdir(parents=True, exist_ok=True)
     images = deal_section(url)
-    for image in images:
+    for image in tqdm(images, os.path.join(os.path.basename(path), name), leave=None, position=current_process()._identity[0]+1):
         logger.info("Downloading %s / %s", name, image)
         download_images(images[image], os.path.join(path, name, image+".jpg"))
         logger.info("Finished %s / %s", name, image)
@@ -120,27 +122,28 @@ def download_book(url: str, path: str, force: bool = False):
 
 
 def download_books(targets: Dict[str, str], path: str, force: bool = False):
-    for item in targets:
+    for item in tqdm(targets, leave=None, position=0):
         download_book(targets[item], os.path.join(path, item), force)
 
 
 def main():
-    target = {
-        "小桃小栗LoveLove物语": "https://dogemanga.com/m/%E5%B0%8F%E6%A1%83%E5%B0%8F%E6%85%84-Love-Love%E7%89%A9%E8%AA%9E/b1I4KAah",
-        "搖曳百合": "https://dogemanga.com/m/%E6%90%96%E6%9B%B3%E7%99%BE%E5%90%88/ezAev-ZJ",
-        "星期一的丰满": "https://dogemanga.com/m/%E6%98%9F%E6%9C%9F%E4%B8%80%E7%9A%84%E4%B8%B0%E6%BB%A1/UlpyE4NI",
-        "3月的狮子": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90/GLursg_Z",
-        "3月的狮子-单行本": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90-%E5%96%AE%E8%A1%8C%E6%9C%AC/w5CcDgbK"
-    }
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
-    logger = logging.getLogger()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", type=str)
-    parser.add_argument("--force", type=bool, default=False)
-    args = parser.parse_args()
-    path = args.path
-    download_books(target, path, args.force)
+    with logging_redirect_tqdm():
+        target = {
+            "小桃小栗LoveLove物语": "https://dogemanga.com/m/%E5%B0%8F%E6%A1%83%E5%B0%8F%E6%85%84-Love-Love%E7%89%A9%E8%AA%9E/b1I4KAah",
+            "搖曳百合": "https://dogemanga.com/m/%E6%90%96%E6%9B%B3%E7%99%BE%E5%90%88/ezAev-ZJ",
+            "星期一的丰满": "https://dogemanga.com/m/%E6%98%9F%E6%9C%9F%E4%B8%80%E7%9A%84%E4%B8%B0%E6%BB%A1/UlpyE4NI",
+            "3月的狮子": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90/GLursg_Z",
+            "3月的狮子-单行本": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90-%E5%96%AE%E8%A1%8C%E6%9C%AC/w5CcDgbK"
+        }
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
+        logger = logging.getLogger()
+        parser = argparse.ArgumentParser()
+        parser.add_argument("path", type=str)
+        parser.add_argument("--force", type=bool, default=False)
+        args = parser.parse_args()
+        path = args.path
+        download_books(target, path, args.force)
 
 
 if __name__ == "__main__":
