026249b6e3e25561834fe055ba349de9c5614778
huguang@WSL.HG-Desktop <1395943920@qq.com>
2021-12-05 22:34:59 +0800

    upd

--- Scrpit/Spider/com.dogemanga/wget-images.py
+++ Scrpit/Spider/com.dogemanga/wget-images.py
@@ -1,12 +1,13 @@
 import logging
 import os
 from pathlib import Path
-from typing import Dict, List,  Optional, Tuple
+from typing import Dict, List,  Optional, Tuple, Type
 import requests
 from lxml import etree
 import argparse
 from retry import retry
 from multiprocessing import Pool, current_process
+from multiprocessing.pool import MapResult, Pool as PoolType
 from functools import partial
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
@@ -21,7 +22,7 @@ class HTTPException(Exception):
 def deal_section(url: str) -> Dict[str, str]:
     logger = logging.getLogger("deal_section")
     logger.info("Fetching image url from section url: %s" % url)
-    resp = requests.get(url, timeout=10)
+    resp = requests.get(url, timeout=(5, 10))
     if not resp.status_code == 200:
         raise HTTPException("not resp.status_code == 200")
     html = resp.text
@@ -48,7 +49,7 @@ def deal_section(url: str) -> Dict[str, str]:
 def deal_book(url: str) -> Dict[str, str]:
     logger = logging.getLogger("deal_book")
     logger.info("Fetching book index from url: %s" % url)
-    resp = requests.get(url, timeout=10)
+    resp = requests.get(url, timeout=(5, 10))
     if not resp.status_code == 200:
         raise HTTPException("not resp.status_code == 200")
     html = resp.text
@@ -74,7 +75,7 @@ def download_images(url: str, path: str):
     Path(dir).mkdir(parents=True, exist_ok=True)
     logger.debug("mkdir -P %s" % dir)
     with open(path, "wb") as f:
-        resp = requests.get(url, timeout=10)
+        resp = requests.get(url, timeout=(5, 10))
         if not resp.status_code == 200:
             raise HTTPException("not resp.status_code == 200")
         logger.debug("Image size = %d" % len(resp.content))
@@ -89,7 +90,7 @@ def download_section(path: str, name: str, url: str):
     logger.info("Start Downloading section url %s to %s" % (name, path))
     Path(path).mkdir(parents=True, exist_ok=True)
     images = deal_section(url)
-    for image in tqdm(images, os.path.join(os.path.basename(path), name), leave=None, position=current_process()._identity[0]+1):
+    for image in tqdm(images, os.path.join(os.path.basename(path), name), leave=False, position=current_process()._identity[0]+1):
         logger.info("Downloading %s / %s", name, image)
         download_images(images[image], os.path.join(path, name, image+".jpg"))
         logger.info("Finished %s / %s", name, image)
@@ -101,7 +102,7 @@ def download_section_s(path: str, x: Tuple[str, str]):
     return download_section(path, x[0], x[1])
 
 
-def download_book(url: str, path: str, force: bool = False):
+def download_book(url: str, path: str, pool: PoolType, force: bool = False):
     logger = logging.getLogger("download_book")
     sections = deal_book(url)
     if not force:
@@ -117,33 +118,37 @@ def download_book(url: str, path: str, force: bool = False):
     #     for image in images:
     #         logger.info("Downloading %s / %s",section,image)
     #         download_images(images[image],os.path.join(args.path,section,image+".jpg"))
-    with Pool(8) as pool:
-        pool.map(partial(download_section_s, path), sections.items())
+    return pool.map_async(partial(download_section_s, path), sections.items())
 
 
-def download_books(targets: Dict[str, str], path: str, force: bool = False):
+def download_books(targets: Dict[str, str], path: str, pool: PoolType, force: bool = False):
+    promises: List[MapResult[None]] = []
     for item in tqdm(targets, leave=None, position=0):
-        download_book(targets[item], os.path.join(path, item), force)
+        promises.append(download_book(
+            targets[item], os.path.join(path, item), pool, force))
+    for p in promises:
+        p.wait()
 
 
 def main():
     with logging_redirect_tqdm():
-        target = {
-            "小桃小栗LoveLove物语": "https://dogemanga.com/m/%E5%B0%8F%E6%A1%83%E5%B0%8F%E6%85%84-Love-Love%E7%89%A9%E8%AA%9E/b1I4KAah",
-            "搖曳百合": "https://dogemanga.com/m/%E6%90%96%E6%9B%B3%E7%99%BE%E5%90%88/ezAev-ZJ",
-            "星期一的丰满": "https://dogemanga.com/m/%E6%98%9F%E6%9C%9F%E4%B8%80%E7%9A%84%E4%B8%B0%E6%BB%A1/UlpyE4NI",
-            "3月的狮子": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90/GLursg_Z",
-            "3月的狮子-单行本": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90-%E5%96%AE%E8%A1%8C%E6%9C%AC/w5CcDgbK"
-        }
-        logging.basicConfig(
-            level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
-        logger = logging.getLogger()
-        parser = argparse.ArgumentParser()
-        parser.add_argument("path", type=str)
-        parser.add_argument("--force", type=bool, default=False)
-        args = parser.parse_args()
-        path = args.path
-        download_books(target, path, args.force)
+        with Pool(16) as pool:
+            target = {
+                "小桃小栗LoveLove物语": "https://dogemanga.com/m/%E5%B0%8F%E6%A1%83%E5%B0%8F%E6%85%84-Love-Love%E7%89%A9%E8%AA%9E/b1I4KAah",
+                "搖曳百合": "https://dogemanga.com/m/%E6%90%96%E6%9B%B3%E7%99%BE%E5%90%88/ezAev-ZJ",
+                "星期一的丰满": "https://dogemanga.com/m/%E6%98%9F%E6%9C%9F%E4%B8%80%E7%9A%84%E4%B8%B0%E6%BB%A1/UlpyE4NI",
+                "3月的狮子": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90/GLursg_Z",
+                "3月的狮子-单行本": "https://dogemanga.com/m/3%E6%9C%88%E7%9A%84%E7%8D%85%E5%AD%90-%E5%96%AE%E8%A1%8C%E6%9C%AC/w5CcDgbK"
+            }
+            logging.basicConfig(
+                level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
+            # logger = logging.getLogger()
+            parser = argparse.ArgumentParser()
+            parser.add_argument("path", type=str)
+            parser.add_argument("--force", type=bool, default=False)
+            args = parser.parse_args()
+            path = args.path
+            download_books(target, path, pool, args.force)
 
 
 if __name__ == "__main__":
